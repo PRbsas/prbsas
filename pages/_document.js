337a659000fc47cd6178bb33e90ac590e61c40e3
PRbsas <paularamirez0316@gmail.com>
2018-07-05 16:35:06 -0400

    Update dependencies and refactor document

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,27 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import styleSheet from 'styled-components/lib/models/StyleSheet'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps ({ renderPage, theme, onToggleTheme }) {
-    const page = renderPage()
-    const styles = (
-      <style dangerouslySetInnerHTML={{ __html: styleSheet.rules().map(rule => rule.cssText).join('\n') }} />
-    )
-
-    return { ...page, styles }
+    const sheet = new ServerStyleSheet()
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const styleTags = sheet.getStyleElement()
+    return { ...page, styleTags }
   }
 
   render () {
     return (
-      <html lang="en">
+      <html lang='en'>
         <Head>
-          <meta charset="utf-8" />
-          <meta http-equiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charset='utf-8' />
+          <meta http-equiv='x-ua-compatible' content='ie=edge' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
           <title>prbsas</title>
-          <meta name="description" content="Portfolio website" />
-          <meta name="distribution" content="Global" />
-          <meta name="theme-color" content="#fff" />
-          <link rel="icon" href="../static/black-circle.png"/>
+          <meta name='description' content='Portfolio website' />
+          <meta name='distribution' content='Global' />
+          <meta name='theme-color' content='#fff' />
+          <link rel='icon' href='../static/black-circle.png' />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
