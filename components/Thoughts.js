6b4f78af80ba26b5057ff7a4d708fab7248cd66d
PRbsas <paularamirez0316@gmail.com>
2018-07-08 16:02:14 -0400

    Refactor thoughts list to display date, style and sort by date

--- components/Thoughts.js
+++ components/Thoughts.js
@@ -8,32 +8,51 @@ import { thoughts } from '../data/thoughts.json'
 const Title = styled.h3`
   cursor: pointer;
   margin: 0 auto;
-  font-size: 32px;
-  font-weight: 400;
-
-  &:hover {
-    font-weight: 800;
-  }
-
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 1.25;
+  margin-top: 32px;
+  margin-bottom: 16px;
   ${media.phone`
       padding-left: 50px;
     `}
 `
 
+const Date = styled.p`
+  font-family: "SF Mono","Roboto Mono",Menlo,monospace;
+  color: inherit;
+  margin-right: 20px;
+  ${media.phone`
+      margin-top: 10px;
+      display: flex;
+      float: right;
+    `}
+`
+
+const Container = styled.div`
+  margin-bottom: 64px;
+`
+
+const thoughtsByDate = thoughts.reverse()
+
 export default () => (
   <Wrapper>
     {
-      thoughts.map(({ id, title }) => (
+      thoughtsByDate.map(({ id, title, date }) => (
         <Thoughts
           id={id}
           key={id}
           title={title}
+          date={date}
         />
       ))
     }
   </Wrapper>
 )
 
-const Thoughts = ({ id, title }) => (
-  <Link prefetch href={`/thoughts/${id}`}><Title>{ title }</Title></Link>
+const Thoughts = ({ id, title, date }) => (
+  <Container>
+    <Link prefetch href={`/thoughts/${id}`}><Title>{ title }</Title></Link>
+    <Date>{date}</Date>
+  </Container>
 )
